298d394fd69287402caac81d479a49162c1806e4
Agalos <georgeagalianos@gmail.com>
2023-03-30 20:55:02 +0300

    first commit

---
+++ first.js
@@ -0,0 +1,41 @@
+//const { EmbedBuilder, WebhookClient, time } = require('discord.js');
+// const { webhookId, webhookToken } = require('./config.json');
+
+
+// const webhookClient = new webhookClient({ url: 'https://discord.com/api/webhooks/899290982204448809/KU4xRnqlsR4o9az1yuun6jmdiPfp5KNBthhhTWZCxpoDXioz9hk4Kut8thV8uUZEgEhy' });
+
+//const webhookClient = new WebhookClient({ url: 'https://discord.com/api/webhooks/1051540143682617384/ANPm4CRayHwmQv9dW5H-xKQLaapnkZnTpnxnDBSn253H5Lo56XXLSO0U75wP9BhRBa9e' });
+
+document.querySelector('#tab_all')
+
+// const embed = new EmbedBuilder()
+// 	.setTitle('Some Title')
+// 	.setColor(0x00FFFF);
+
+// webhookClient.send({
+// 	content: 'Webhook test',
+// 	username: 'some-username',
+// 	avatarURL: 'https://i.imgur.com/AfFp7pu.png',
+// 	embeds: [embed],
+// });
+
+
+///////////////////////////////////////////////
+// sleepTime = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
+// counter = 0
+
+// while (true) {
+// 	if (document.getElementById('command_overview').innerText == "Δεν υπάρχουν τώρα κινήσεις του στρατεύματος.") {
+// 		sleep(sleepTime);
+// 	} else {
+// 		childs = document.getElementById('command_overview').childNodes //get all the values from the movements window
+// 		sleep(sleepTime/2)
+// 		for (i in childs) {
+// 			if (i.id.startsWith('command')) {
+
+// 			}
+// 		}
+
+// 	}
+// }
+
